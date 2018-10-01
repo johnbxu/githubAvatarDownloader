10070d1033e9817f443eda96e8b9942e3db3ae1d
johnbxu <johnbxu@gmail.com>
2018-10-01 16:38:02 +0000

    added gitignore and put secrets into it

--- download_avatars.js
+++ download_avatars.js
@@ -3,6 +3,20 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  let options = {
+    // url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    url: `https://github.com/lighthouse-labs/promises-exercises`,
+    headers: {
+      'User-Agent': 'request',
+
+    },
+  };
+
+  request(options.url, function(err, res, body) {
+    cb(err, body);
+  });
+
+
 
 }
 
