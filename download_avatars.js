cceff512c437d72f61d7122e03ab65effc34748f
johnbxu <johnbxu@gmail.com>
2018-10-01 17:25:55 +0000

    added downloadImageByURL function and made it work with a single URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
-var request = require('request');
-var token = require('./secrets.js');
+let request = require('request');
+let token = require('./secrets.js');
+let fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -18,14 +19,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
     let parsedList = JSON.parse(body);
     cb(err, parsedList);
   });
-
-
-
 }
-
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
+
   for (contributor of result) {
     console.log("\n------------------------------");
     console.log("Contributor:", contributor.login);
@@ -33,3 +31,24 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   }
 });
 
+function downloadImageByURL(url, filePath) {
+  let options = {
+    url: `${url}`,
+    // url: `https://github.com/lighthouse-labs/promises-exercises`,
+    headers: {
+      'User-Agent': 'request',
+      'Authentication': token,
+
+    },
+  };
+  request.get(options.url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('response status code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
