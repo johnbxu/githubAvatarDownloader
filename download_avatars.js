94acd2460320aec2f0c89b5bea1e061794b880f2
johnbxu <johnbxu@gmail.com>
2018-10-01 16:58:48 +0000

    added code to download_avatars to make it work with headers and auth token

--- download_avatars.js
+++ download_avatars.js
@@ -1,19 +1,22 @@
 var request = require('request');
+var token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   let options = {
-    // url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
-    url: `https://github.com/lighthouse-labs/promises-exercises`,
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    // url: `https://github.com/lighthouse-labs/promises-exercises`,
     headers: {
       'User-Agent': 'request',
+      'Authentication': token,
 
     },
   };
 
-  request(options.url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    let parsedList = JSON.parse(body);
+    cb(err, parsedList);
   });
 
 
@@ -25,3 +28,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
